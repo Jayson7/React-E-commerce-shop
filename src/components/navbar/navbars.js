ee33bfc510557b276c12d8890619119c4fcf3777
jayson7 <lexxiijoo70@gmail.com>
2021-11-13 17:31:54 +0100

    react ecommerce navbar responsiveness

--- src/components/navbar/navbars.js
+++ src/components/navbar/navbars.js
@@ -1,6 +1,10 @@
-var toggle = document.querySelector(".toggle");
-var times = document.querySelector(".times");
-var navbar = document.querySelector("nav");
-console.log(toggle);
-console.log(times);
-console.log(navbar);
+window.addEventListener("load", () => {
+  var toggle = document.querySelector(".toggle");
+  var times = document.querySelector(".times");
+  var navbar = document.querySelector("nav");
+  toggle.addEventListener("click", () => {
+    toggle.style.display = "none";
+    navbar.style.display = "flex";
+  });
+  times.addEventListener("click", () => {});
+});
