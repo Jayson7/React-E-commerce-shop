43d193cfed9cd18fcf5d44a741541d0bd0e5b3f6
jayson7 <lexxiijoo70@gmail.com>
2021-11-14 15:05:50 +0100

    front page

--- src/components/homepage/home.js
+++ src/components/homepage/home.js
@@ -1,5 +1,5 @@
-var texts = document.getElementsByClassName("text1");
-console.log(texts);
+var texts = document.querySelectorAll(".text1")[0];
+// console.log(texts.innerText);
 var textreplace = new Array(
   "Our Customers are the best",
   "Our clients Bank on our integrity and reliability",
@@ -7,14 +7,14 @@ var textreplace = new Array(
   "Quality over quantity, all day all hour"
 );
 
-var len = texts.length;
+var len = textreplace.length;
 var i = 0;
 
 function textslider() {
   if (i > len - 1) {
     i = 0;
   }
-  texts.text = textreplace[i];
+  texts.innerText = textreplace[i];
   i++;
   setTimeout("textslider()", 3000);
 }
