9340068fd747709e7b7114d16a7e54804247916b
jayson7 <lexxiijoo70@gmail.com>
2021-11-14 17:15:18 +0100

    front page

--- src/components/homepage/home.jsx
+++ src/components/homepage/home.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./home.css";
 import "./home";
-// import textslider from "./home";
+import textslider from "./home";
 
 export class Home extends Component {
   render() {
@@ -9,12 +9,21 @@ export class Home extends Component {
       <div>
         <div className="home-container">
           <div className="grab">
-            <div className="image">
-              <img src="" className="image1" alt="" />
-            </div>
             <div className="texthome">
               <h1 className="text1"> We sell Explicit products </h1>
+              <h6>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga
+                dolores accusantium sequi! Adipisci.
+              </h6>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Voluptatum dolorum quos quidem officia earum eligendi! Unde
+                quisquam pariatur voluptatibus facilis labore optio minima,
+                ratione quam iusto quo repellendus adipisci excepturi.
+              </p>
             </div>
+            <button>Cart</button>
+            <button>Shop</button>
           </div>
         </div>
       </div>
